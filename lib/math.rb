8be21b58353d3e4f74026d06b1384b039fdf8056
Barbara Kuofie <barbara.kuofie@gmail.com>
2020-08-27 23:44:53 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,13 +1,16 @@
 def addition(num1, num2)
-
+sum = num1 + num2
+return sum 
 end
 
 def subtraction(num1, num2)
-
+difference = num1 - num2
+return difference
 end
 
 def division(num1, num2)
-
+div = num1 / num2
+return div 
 end
 
 def multiplication(num1, num2)
